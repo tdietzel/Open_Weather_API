3964f513a865c44c57370af62152f24457cc676b
tdietzel22 <147360049+tdietzel22@users.noreply.github.com>
2023-11-14 09:49:06 -0800

    Add business and UI logic for full weather forcast in °F by city

--- src/index.js
+++ src/index.js
@@ -1,3 +1,50 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+
+// Business Logic
+
+function getWeather(city) {
+  let request = new XMLHttpRequest();
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=imperial`;
+  
+  request.addEventListener("loadend", function() {
+    const response = JSON.parse(this.responseText);
+    console.log(response);
+    if (this.status === 200) {
+      printElements(response, city);
+    } else {
+      printError(this, response, city);
+    }
+  });
+
+  request.open("GET", url, true);
+  request.send();
+}
+
+// UI Logic
+
+function printError(request, apiResponse, city) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the weather data for ${city}: ${request.status} ${request.statusText}: ${apiResponse.message}`;
+}
+
+function printElements(apiResponse, city) {
+  document.querySelector('#showResponse').innerText =
+  `The humidity in ${city} is ${apiResponse.main.humidity}%.
+  The temperature is ${apiResponse.main.temp}°F.
+  The temperature feels like ${apiResponse.main.feels_like}°F
+  Daily High: ${apiResponse.main.temp_max}°F
+  Daily Low: ${apiResponse.main.temp_min}°F
+  Wind Speed: ${apiResponse.wind.speed}ᴍᴘʜ`;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const city = document.querySelector('#location').value;
+  document.querySelector('#location').value = null;
+  getWeather(city);
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
